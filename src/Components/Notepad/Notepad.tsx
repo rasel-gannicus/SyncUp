a1109307207902bf6e9cb6f7fa3733c009534e46
shafik720 <shafikrasel5@gmail.com>
2024-10-03 23:37:27 +0600

    adding navbar

---
+++ src/Components/Notepad/Notepad.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Notepad = () => {
+    return (
+        <div>
+            <h2>Add Note pad here </h2>
+        </div>
+    );
+};
+
+export default Notepad;
---
+++ src/app/notepad/page.tsx
@@ -0,0 +1,12 @@
+import Notepad from '@/components/Notepad/Notepad';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <Notepad /> 
+        </div>
+    );
+};
+
+export default page;
